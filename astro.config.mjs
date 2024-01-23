9b5b34bd7992b086985093608f04b6cc11f18f20
Juanjo Martínez <juanjodevs@gmail.com>
2024-01-23 10:05:52 +0100

    Added profile photo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
+  output: 'server',
   adapter: cloudflare({
     imageService: 'compile'
   })
-});
+})
