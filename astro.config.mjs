176cf8f00745db098b88a6594a46ff340eb7c122
Juanjo Martínez <juanjodevs@gmail.com>
2024-05-21 15:12:29 +0200

    Vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
-import cloudflare from '@astrojs/cloudflare'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'hybrid',
-  adapter: cloudflare({
-    imageService: 'compile'
+  adapter: vercel({
+    webAnalytics: { enabled: true }
   })
 })
