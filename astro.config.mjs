84e92e14517a59bb47176ec1c60f7a34dc2e8ae4
Juanjo Martínez <juanjodevs@gmail.com>
2024-10-24 11:34:05 +0200

    Blog stash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
 
 import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }),
+  mdx()],
   output: 'hybrid',
   adapter: vercel({
     webAnalytics: { enabled: true }
